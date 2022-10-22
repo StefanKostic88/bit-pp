71f56893b707b605a83996cce75286c9b1173ed9
stefanKostic88 <stefankostic88@yahoo.com>
2022-10-22 14:03:39 +0200

    array exe v1

---
+++ JS/Arrays/arrayExe.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+// 1. Write an array of months in a year. Using console.log display June, October and January
+// from the array.
+
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "Jun",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+console.log(months[0], months[5], months[9]);
+*/
+
+/*
+// 2. Write an arrays of days in a week. Using console.log display Sunday from the array.
+
+var weekDays = [
+  "Monday",
+  "Thusday",
+  "Wendsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+console.log(weekDays[6]);
+*/
+/*
+// 3. Print all negative elements from the array [2, -4, 5, -2, -11].
+var arr = [2, -4, 5, -2, -11];
+console.log(arr[1], arr[3], arr[4]);
+*/
+/*
+// 4. What is the index of number 24 in the following array?
+var arr = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+];
+
+console.log(arr[0][3]);
+console.log(arr[2][1]);
+*/
