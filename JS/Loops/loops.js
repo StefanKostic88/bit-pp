0a4f36b2e36c10275acad0d815c0a94eca2cd950
StefanKostic88 <stefankostic88@yahoo.com>
2022-10-25 20:14:53 +0200

    loops

---
+++ JS/Loops/loops.js
@@ -0,0 +1,57 @@
+"use strict";
+//vise petlji
+//4 petlje za sada
+
+// while, do while, for, for-in
+
+//for-in se koristi za obj
+
+// while
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// do-while
+
+// let i = 3;
+
+// // izvrsava se sigurno barem jednom cak i ako uslov nije ispunjen
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+//i === 21;
+
+// const cars = ["bmw", "audi"];
+
+// let i = 0;
+// let text = "";
+
+// while (cars[i]) {
+//   text += cars[i] + ", ";
+//   i++;
+// }
+
+// console.log(text);
+
+//for // isto sto i while petlja samo drugacija sintaksa
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(i);
+// }
+
+// let res = "";
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     res += "*\t";
+//     // console.log(res);
+//   }
+//   // console.log(res);
+//   res += "\n";
+// }
+// console.log(res);
