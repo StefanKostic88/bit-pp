150806a1f85cb4e60ea1f42c33d425e6e2214dc6
stefanKostic88 <stefankostic88@yahoo.com>
2023-01-24 18:03:16 +0100

    update 1.02

--- JS/MVC/exam/js/app.js
+++ JS/MVC/exam/js/app.js
@@ -3,6 +3,15 @@ var subjectName = document.getElementById("subject-name");
 var studentNameEl = document.getElementById("student-name");
 var studentGradeEl = document.getElementById("student-grade");
 
+var passedListEl = document.querySelector(".passed-list");
+var faildListEl = document.querySelector(".faild-list");
+
+var passedNumEl = document.querySelector("#passed-number");
+var faildNumEl = document.querySelector("#faild-number");
+
+var passedNumber = 0;
+var faildNumber = 0;
+
 submitInfoEl.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -23,6 +32,40 @@ submitInfoEl.addEventListener("click", function (e) {
   var form = new Form(exam);
   console.log(form);
 
-  //   console.log(exam.hasPassed());
-  //   console.log(exam.getExamInfo());
+  ///u form class ubaciti metodu za validaciju
+
+  var hasPassed = exam.hasPassed();
+  console.log(hasPassed);
+
+  if (hasPassed) {
+    passedNumber++;
+    passedNumEl.textContent = passedNumber;
+    var tempLi = document.createElement("li");
+    var tempSpan = document.createElement("span");
+    tempSpan.setAttribute("class", "list-item-name-passed");
+    var tempSpan2 = document.createElement("span");
+    tempSpan2.setAttribute("class", "list-item-grade-passed");
+    tempSpan.textContent =
+      subject.getSubjectName() + " " + student.getStudentData();
+
+    tempSpan2.textContent = grade;
+    tempLi.append(tempSpan);
+    tempLi.append(tempSpan2);
+    passedListEl.append(tempLi);
+  } else {
+    faildNumber++;
+    faildNumEl.textContent = faildNumber;
+
+    var tempLi = document.createElement("li");
+    var tempSpan = document.createElement("span");
+    tempSpan.setAttribute("class", "list-item-name-faild");
+    var tempSpan2 = document.createElement("span");
+    tempSpan2.setAttribute("class", "list-item-grade-faild");
+    tempSpan.textContent =
+      subject.getSubjectName() + " " + student.getStudentData();
+    tempSpan2.textContent = grade;
+    tempLi.append(tempSpan);
+    tempLi.append(tempSpan2);
+    faildListEl.append(tempLi);
+  }
 });
