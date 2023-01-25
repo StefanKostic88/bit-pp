696f2e7285fb35dccc63e749eab32e1676d54fbc
stefanKostic88 <stefankostic88@yahoo.com>
2023-01-25 20:02:54 +0100

    exam exe completed

--- JS/MVC/exam/js/app.js
+++ JS/MVC/exam/js/app.js
@@ -1,20 +1,16 @@
 var app = new App();
 
-var subjectName = document.getElementById("subject-name");
-var studentNameEl = document.getElementById("student-name");
-var studentGradeEl = document.getElementById("student-grade");
-
-var controlForm = function () {
+var controlForm = function (subjectName, studentName, studentGrade) {
   var subject = new Subject(subjectName.value);
-  var student = new StudentName(studentNameEl.value);
-  var grade = studentGradeEl.value;
+  var student = new StudentName(studentName.value);
+  var grade = studentGrade.value;
   if (+grade > 10 || +grade < 1) {
-    studentGradeEl.style.backgroundColor = "red";
-    studentGradeEl.placeholder = "number must be between 1 and 10";
+    studentGrade.style.backgroundColor = "red";
+    studentGrade.placeholder = "number must be between 1 and 10";
     return;
   } else {
-    studentGradeEl.style.backgroundColor = "";
-    studentGradeEl.placeholder = "grade";
+    studentGrade.style.backgroundColor = "";
+    studentGrade.placeholder = "grade";
   }
   var exam = new Exam(subject, student, grade);
   var form = new Form(exam);
@@ -25,9 +21,6 @@ var controlForm = function () {
   } else {
     app.fail = form.faildExam(app.fail);
   }
-  subjectName.value = "";
-  studentNameEl.value = "";
-  studentGradeEl.value = "";
 };
 
 app.controlFormHandler(controlForm);
--- JS/MVC/exam/js/appClass.js
+++ JS/MVC/exam/js/appClass.js
@@ -7,8 +7,14 @@ class App {
 
   controlFormHandler(handler) {
     this._submitInfoEl.addEventListener("click", function (e) {
+      var subjectNameEL = document.getElementById("subject-name");
+      var studentNameEl = document.getElementById("student-name");
+      var studentGradeEl = document.getElementById("student-grade");
       e.preventDefault();
-      handler();
+      handler(subjectNameEL, studentNameEl, studentGradeEl);
+      subjectNameEL.value = "";
+      studentNameEl.value = "";
+      studentGradeEl.value = "";
     });
   }
 }
