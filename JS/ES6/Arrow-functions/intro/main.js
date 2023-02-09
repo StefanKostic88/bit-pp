7e976e2ab4754f09b0fda4807bd5011c78ef05cf
StefanKostic88 <stefankostic88@yahoo.com>
2023-02-09 20:09:57 +0100

    reduce

--- JS/ES6/Arrow-functions/intro/main.js
+++ JS/ES6/Arrow-functions/intro/main.js
@@ -37,3 +37,73 @@ console.log(objN);
 const newValue = (...data) => data.reduce((acc, cur) => acc + cur, 0);
 
 console.log(newValue(1, 2, 3, 4, 5));
+
+let users = [
+  { userName: "Pera", lastName: "Peric", id: "54ds0" },
+  { userName: "Pera", lastName: "Peric", id: "54ds0" },
+  { userName: "asdasd", lastName: "Peric", id: "asdas" },
+  // { userName: "Pera", lastName: "Peric", id: "54asdds0" },
+  { userName: "Pera", lastName: "Peric", id: "54ds0" },
+  { userName: "Pera", lastName: "Peric", id: "54asdds0" },
+];
+
+const newUsersIdMap = users.reduce((acc, cur) => {
+  acc[cur.id] = cur;
+  return acc;
+}, {});
+
+console.log(newUsersIdMap["54ds0"], "a");
+
+/*
+let users = [
+  {
+    name: 'Pera',
+    lastName: 'Peric',
+    id: '1df3',
+  },
+  {
+    name: 'marko',
+    lastName: 'markovic',
+    id: '22rf',
+  },
+  {
+    name: 'jovana',
+    lastName: 'jovanovic',
+    id: 'gf55',
+  },
+  {
+    name: 'stefan',
+    lastName: 'stefanovic',
+    id: 'kio0',
+  },
+];
+
+// usersMap = {
+// 	'1df3': {
+// 		name: 'Pera',
+// 		lastName: 'Peric',
+// 		id: '1df3',
+// 	},
+// 	'22rf': {
+// 		name: 'marko',
+// 		lastName: 'markovic',
+// 		id: '22rf',
+// 	},
+// 	'gf55': {
+// 		name: 'jovana',
+// 		lastName: 'jovanovic',
+// 		id: 'gf55',
+// 	},
+// 	'kio0': {
+// 		name: 'stefan',
+// 		lastName: 'stefanovic',
+// 		id: 'kio0',
+// 	}
+// }
+
+const usersIdMap = users.reduce(function (acc, curr) {
+  acc[curr.id] = curr;
+  return acc;
+}, {});
+
+console.log(usersIdMap['kio0']);*/
