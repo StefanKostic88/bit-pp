e20e7a935e713d1f64ef1fff3f864aab7e22dbed
StefanKostic88 <stefankostic88@yahoo.com>
2023-03-02 19:43:57 +0100

    react intro

---
+++ React/Intro/main.js
@@ -0,0 +1,29 @@
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+
+// const helloSpanEl = React.createElement("span", { className: "span" }, "Hello");
+// const worldSpanEl = React.createElement("span", { className: "span" }, "World");
+
+// const paragraphReactElement = React.createElement(
+//   "p",
+//   { className: "paragraph" },
+//   helloSpanEl,
+//   " ",
+//   worldSpanEl
+// );
+
+// root.render(paragraphReactElement);
+
+//drugi nacin
+
+// const paraElement = (
+//   <ul>
+//     <li>Item 1</li>
+//     <li>Item 2</li>
+//     <li>Item 3</li>
+//     <li>Item 4</li>
+//   </ul>
+// );
+
+// root.render(paraElement);
